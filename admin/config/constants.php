b360c48abbf0531c16c698ce62c2a157dd024f57
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-28 22:54:17 +0545

    Update constants.php

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -8,7 +8,7 @@ define("ROOT_URL", "http://rishikblog.vercel.app/");
 define('DB_HOST', 'sql304.infinityfree.com');
 define('DB_USER', 'if0_36312985');
 define('DB_PASS', 'H0iCohPotYbx');
-define('DB_NAME', 'if0_36312985_XXX');
+define('DB_NAME', 'if0_36312985_rishik');
 define('DB_PORT', 3306); // Optional, if you want to explicitly set the port
 
 // Check if user is logged in
